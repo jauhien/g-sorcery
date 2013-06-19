0da13d1665f622546d3608c84e1a737552cbcaba
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-06-19 22:58:46 +0200

    g_sorcery/backend.py: initial commit

---
+++ g_sorcery/backend.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    backend.py
+    ~~~~~~~~~~
+    
+    base class for backends
+    
+    :copyright: (c) 2013 by Jauhien Piatlicki
+    :license: GPL-2, see LICENSE for more details.
+"""
+
+class Backend:
+    def __init__(self):
+        pass
