c6f8894af05759513b3baf8b97d1f4805fb2aab3
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-06-23 00:59:04 +0200

    g_sorcery/ebuild.py: initial commit

---
+++ g_sorcery/ebuild.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    ebuild.py
+    ~~~~~~~~~~~~~
+    
+    ebuild generation
+    
+    :copyright: (c) 2013 by Jauhien Piatlicki
+    :license: GPL-2, see LICENSE for more details.
+"""
