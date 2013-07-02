ab7e4b9f72ac3fcf90a04d1743a2823e431ed0ef
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-07-02 16:49:51 +0200

    py2k: tests/server

--- tests/server.py
+++ tests/server.py
@@ -11,13 +11,23 @@
     :license: GPL-2, see LICENSE for more details.
 """
 
-import http.server, threading
+import threading
+
+from g_sorcery.compatibility import py2k
+
+if py2k:
+    from SocketServer import TCPServer as HTTPServer 
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+else:
+    from http.server import HTTPServer
+    from http.server import SimpleHTTPRequestHandler
 
 class Server(threading.Thread):
     def __init__(self):
         super(Server, self).__init__()
+        HTTPServer.allow_reuse_address = True
         server_address = ('127.0.0.1', 8080)
-        self.httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+        self.httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     
     def run(self):
         self.httpd.serve_forever()
