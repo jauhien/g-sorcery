3ec16e18c9d58f78fbdc0b138fe3e13c1f856864
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-07-04 01:39:36 +0200

    g_sorcery/fileutils: fix indent

--- g_sorcery/fileutils.py
+++ g_sorcery/fileutils.py
@@ -119,7 +119,7 @@ def get_pkgpath():
     Returns:
         Package path.
     """
-     root = __file__
-     if os.path.islink(root):
-         root = os.path.realpath(root)
-     return os.path.dirname(os.path.abspath(root))
+    root = __file__
+    if os.path.islink(root):
+        root = os.path.realpath(root)
+    return os.path.dirname(os.path.abspath(root))
