cb6cd5ed08df4b5bbb8fb14d131fa0540edc3f69
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-07-16 23:59:34 +0200

    better template generation

--- g_elpa/ebuild.py
+++ g_elpa/ebuild.py
@@ -11,17 +11,48 @@
     :license: GPL-2, see LICENSE for more details.
 """
 
+import collections
 import os
 
-from g_sorcery.ebuild import EbuildGeneratorFromFile
+from g_sorcery.ebuild import DefaultEbuildGenerator
 from g_sorcery.fileutils import get_pkgpath
 
-class ElpaEbuildWithDigestGenerator(EbuildGeneratorFromFile):
-    def __init__(self, package_db):
-        name = os.path.join(get_pkgpath(__file__), 'data/ebuild_with_digest.tmpl')
-        super(ElpaEbuildWithDigestGenerator, self).__init__(package_db, filename = name)
+Layout = collections.namedtuple("Layout",
+    ["vars_before_inherit", "inherit", "vars_after_description", "vars_after_keywords"])
+  
 
-class ElpaEbuildWithoutDigestGenerator(EbuildGeneratorFromFile):
+class ElpaEbuildWithDigestGenerator(DefaultEbuildGenerator):
     def __init__(self, package_db):
-        name = os.path.join(get_pkgpath(__file__), 'data/ebuild_without_digest.tmpl')
-        super(ElpaEbuildWithoutDigestGenerator, self).__init__(package_db, filename = name)
+
+        vars_before_inherit = \
+          ["repo_uri", "source_type", "realname", ("digest_sources", "yes")]
+
+        inherit = ["g-elpa"]
+        
+        vars_after_description = \
+          ["homepage", ("src_uri", "${REPO_URI}${REALNAME}-${PV}.${SUFFIX}")]
+
+        vars_after_keywords = \
+          ["depend", "rdepend"]
+
+        layout = Layout(vars_before_inherit, inherit, vars_after_description, vars_after_keywords)
+
+        super(ElpaEbuildWithDigestGenerator, self).__init__(package_db, layout)
+
+class ElpaEbuildWithoutDigestGenerator(DefaultEbuildGenerator):
+    def __init__(self, package_db):
+
+        vars_before_inherit = \
+          ["repo_uri", "source_type", "realname", ("digest_sources", "yes")]
+
+        inherit = ["g-elpa"]
+        
+        vars_after_description = \
+          ["homepage"]
+
+        vars_after_keywords = \
+          ["depend", "rdepend"]
+
+        layout = Layout(vars_before_inherit, inherit, vars_after_description, vars_after_keywords)
+
+        super(ElpaEbuildWithoutDigestGenerator, self).__init__(package_db, layout)
