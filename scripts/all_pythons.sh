422bdd968daad001c3cd39d06d2321ba54f635e3
Jauhien Piatlicki (jauhien) <piatlicki@gmail.com>
2013-09-16 00:46:28 +0200

    scripts/all_pythons.sh: show python version

--- scripts/all_pythons.sh
+++ scripts/all_pythons.sh
@@ -4,5 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for VER in 2.7 3.2 3.3 #2.6 to be added later
 do
+    echo
+    echo "testing python${VER}"
     python${VER} ${DIR}/run_tests.py
 done
